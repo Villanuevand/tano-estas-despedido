55e2c387017a0df6d73f6a6b5d037486fb4144d4
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-05 23:05:43 -0300

    Options...

--- js/options.js
+++ js/options.js
@@ -1,3 +1,30 @@
 /**
- * Created by avillanueva on 5/5/16.
+ * @name hidden
+ * @description Change Notification color and frequency
+ * @param isDeactivated
  */
+function hidden(isDeactivated) {
+    options.style.color = isDeactivated ? 'graytext' : 'black';
+
+    options.frequency.disabled = isDeactivated;
+}
+// Listener
+window.addEventListener('load', function() {
+
+    options.isActivated.checked = JSON.parse(localStorage.isActivated);
+
+    options.frequency.value = localStorage.frequency;
+
+
+    if (!options.isActivated.checked) { hidden(true); }
+
+    options.isActivated.onchange = function() {
+        localStorage.isActivated = options.isActivated.checked;
+        hidden(!options.isActivated.checked);
+    };
+
+    options.frequency.onchange = function() {
+        localStorage.frequency = options.frequency.value;
+    };
+});
+
