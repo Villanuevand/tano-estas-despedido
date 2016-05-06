14e519b2583cfd2725fa1cb784031b45785dba12
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-05 23:05:59 -0300

    EventPage...

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,1 +1,36 @@
+function show() {
+    var time = /(..)(:..)/.exec(new Date());
+    var hour = time[1] % 12 || 12;
+    var period = time[1] < 12 ? 'a.m.' : 'p.m.';
+    new Notification(hour + time[2] + ' ' + period, {
+        icon: '48.png',
+        body: '¡Hora de despedir al Tano'
+    });
+}
+
+if (!localStorage.isInitialized) {
+    localStorage.isActivated = true;
+    localStorage.frequency = 1;
+    localStorage.isInitialized = true;
+}
+
+
+if (window.Notification) {
+    
+    if (JSON.parse(localStorage.isActivated)) { show(); }
+
+    var interval = 0;
+
+    setInterval(function() {
+        interval++;
+
+        if (
+            JSON.parse(localStorage.isActivated) &&
+            localStorage.frequency <= interval
+        ) {
+            show();
+            interval = 0;
+        }
+    }, 60000);
+}
 
